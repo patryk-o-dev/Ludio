42f121607a4a47eb99d46f3dbab3d5d6e3c7c8b0
patryk-o-dev <patryk.o.dev@gmail.com>
2026-02-18 06:24:20 +0100

    chore: prisma setup and initial database

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
+  await app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
