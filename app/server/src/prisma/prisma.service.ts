602b9aa77a4b426c0aa85436cccd6c242277439b
patryk-o-dev <patryk.o.dev@gmail.com>
2026-05-03 16:31:46 +0200

    chore: prisma setup and initial database

---
+++ app/server/src/prisma/prisma.service.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@/generated/prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import 'dotenv/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    super({ adapter, log: ['info', 'warn', 'error'] });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$queryRaw`SELECT 1`;
+      console.log('✅ Prisma connected to MySQL');
+    } catch (error) {
+      console.error('❌ Prisma connection error:', error);
+      throw error;
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('🔌 Prisma disconnected from MySQL');
+  }
+}
